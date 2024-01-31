9f860d1568bd8b29dd8fbee466f14ca471cb4166
Gabriel Souza <gabriel.a.souza@outlook.com.br>
2024-01-30 21:49:05 -0300

    adicionando titulo e paragrafo

---
+++ app.js
@@ -0,0 +1,15 @@
+//pegando elementos do arquivo html com js usando o querySelector
+let titulo = document.querySelector('h1');
+
+//inserindo o texto dentro (innerHTML) da tag h1
+titulo.innerHTML = 'Jogo do Número Secreto';
+
+//fazendo o mesmo com a tag p (paragrafo)
+let paragrafo = document.querySelector('p')
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+// sempre que queremos usar o elemento 'onclick' passamos nele o nome da nossa funcao que sera feita aqui no js
+// no arquivo html o nome da nossa funcao eh 'verificarChute', logo, devemos cria-la aqui com o mesmo nome
+function verificarChute() {
+    console.log('o botao foi clicado!')
+}
