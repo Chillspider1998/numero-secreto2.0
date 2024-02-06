51981271c7affd4a06aa6f2911cb84f0870757f8
Gabriel Souza <gabriel.a.souza@outlook.com.br>
2024-02-06 13:15:11 -0300

    adicionando funcoes com parametro

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Jogo do Número Secreto';
 
 //fazendo o mesmo com a tag p (paragrafo)
-let paragrafo = document.querySelector('p')
+let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 // sempre que queremos usar o elemento 'onclick' passamos nele o nome da nossa funcao que sera feita aqui no js
 // no arquivo html o nome da nossa funcao eh 'verificarChute', logo, devemos cria-la aqui com o mesmo nome
 function verificarChute() {
-    console.log('o botao foi clicado!')
+    console.log('o botao foi clicado!');
+}
+
+// criando funcao com parametros
+
+function exibirTexto() {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
