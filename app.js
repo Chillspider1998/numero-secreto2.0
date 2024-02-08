289a631cef73705d9be1caed4001641be2a5ecb2
Gabriel Souza <gabriel.a.souza@outlook.com.br>
2024-02-08 13:11:50 -0300

    JS: explorando funcoes

--- app.js
+++ app.js
@@ -9,16 +9,22 @@
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 // criando funcao com parametros
+let numeroSecreto = gerarNumeroAleatorio();
 
 function exibirTexto(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 exibirTexto('h1', 'Jogo do Número Secreto');
-exibirTexto('p', 'Escolha um número entre 1 e 100');
+exibirTexto('p', 'Escolha um número entre 1 e 10');
 
 // sempre que queremos usar o elemento 'onclick' passamos nele o nome da nossa funcao que sera feita aqui no js
 // no arquivo html o nome da nossa funcao eh 'verificarChute', logo, devemos cria-la aqui com o mesmo nome
 function verificarChute() {
-    console.log('o botao foi clicado!');
+    let chute = document.querySelector('input').value; //estamos pegando o valor digitado dentro do input do html
+    console.log(chute == numeroSecreto);
+}
+
+function gerarNumeroAleatorio () {
+   return parseInt(Math.random() * 10 + 1);
 }
