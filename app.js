b18acc15e3058cc2e9b011e2f8a7e8a688eefea6
Gabriel Souza <gabriel.a.souza@outlook.com.br>
2024-02-21 21:47:07 -0300

    adicionando: funcao limparCampo; funcao mensagemInicial e funcao novoJogo

--- app.js
+++ app.js
@@ -11,13 +11,18 @@
 // criando funcao com parametros
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
+mensagemInicial();
 
 function exibirTexto(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
-exibirTexto('h1', 'Jogo do Número Secreto');
-exibirTexto('p', 'Escolha um número entre 1 e 10');
+//funcao que exibe as mensagens iniciais 
+function mensagemInicial() {
+    exibirTexto('h1', 'Jogo do Número Secreto');
+    exibirTexto('p', 'Escolha um número entre 1 e 10');
+}
+
 
 // sempre que queremos usar o elemento 'onclick' passamos nele o nome da nossa funcao que sera feita aqui no js
 // no arquivo html o nome da nossa funcao eh 'verificarChute', logo, devemos cria-la aqui com o mesmo nome
@@ -28,6 +33,7 @@ function verificarChute() {
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o numero secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTexto('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     }else {
         if (chute > numeroSecreto) {
             exibirTexto('p', 'O numero secreto e menor.');
@@ -35,9 +41,24 @@ function verificarChute() {
             exibirTexto('p', 'O numero secreto e maior');
         }
         tentativas++;
+        limparCampo();
     }
 }
 
 function gerarNumeroAleatorio () {
    return parseInt(Math.random() * 10 + 1);
+}
+
+//funcao que limpa o input sempre que o usuario realiza uma tentativa
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function novoJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    mensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
